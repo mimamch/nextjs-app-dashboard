a6779e066018a6be42475709e69b242f296099cd
mimamch <mimamch28@gmail.com>
2023-05-17 12:15:35 +0700

    changes

--- ui/headers/index.tsx
+++ ui/headers/index.tsx
@@ -1,16 +1,14 @@
 import { UserCircleIcon, MoonIcon } from '@heroicons/react/solid';
 import React from 'react';
+import AvatarIcon from '../partials/AvatarIcon';
 import DarkmodeButton from './darkmode-button';
-import Button from '../button';
 
 export default function GlobalHeader() {
   return (
     <div className="fixed z-10 flex h-14 w-full items-center justify-end rounded-b-2xl bg-white  pr-3 shadow dark:bg-black md:pl-64 ">
       <DarkmodeButton />
-      <button className="flex items-center gap-2">
-        <span className="text-sm text-gray-400">mimamch</span>
-        <UserCircleIcon className=" block w-10 text-gray-500 dark:text-gray-400" />
-      </button>
+
+      <AvatarIcon />
     </div>
   );
 }
---
+++ ui/partials/AvatarIcon.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React from 'react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import * as Avatar from '@radix-ui/react-avatar';
+
+export default function AvatarIcon() {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Avatar.Root className="inline-flex h-10 w-10 select-none items-center justify-center overflow-hidden rounded-full align-middle">
+          <Avatar.Image
+            className="h-full w-full rounded-[inherit] object-cover"
+            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+            alt="Colm Tuite"
+          />
+          <Avatar.Fallback
+            className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+            delayMs={600}
+          >
+            MIC
+          </Avatar.Fallback>
+        </Avatar.Root>
+      </DropdownMenu.Trigger>
+      {/* <DropdownMenu.Portal> */}
+      <DropdownMenu.Content
+        className=" min-w-[220px] rounded-md bg-white px-2 py-2 dark:bg-gray-800"
+        sideOffset={5}
+      >
+        <DropdownMenu.Item className="group relative flex h-[25px] select-none items-center rounded ">
+          <button className="w-full text-left">Log Out</button>
+        </DropdownMenu.Item>
+
+        <DropdownMenu.Arrow className="fill-white dark:fill-gray-500" />
+      </DropdownMenu.Content>
+      {/* </DropdownMenu.Portal> */}
+    </DropdownMenu.Root>
+  );
+}
